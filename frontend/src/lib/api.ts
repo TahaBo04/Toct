7c0b5503cabb5cf2fc52313e6ec1d8efb0caed3e
TahaBo04 <tahab896@gmail.com>
2025-09-07 20:16:50 +0100

    Create api.ts

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,7 @@
+const BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8000";
+
+export async function api<T>(path: string, init?: RequestInit): Promise<T> {
+  const res = await fetch(`${BASE}${path}`, { cache: "no-store", ...init });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+}
